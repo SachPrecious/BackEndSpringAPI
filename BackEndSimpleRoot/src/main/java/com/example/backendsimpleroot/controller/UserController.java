44cde765cb7d25bd5e437b35b2c56f61889b652b
SachPrecious <sachithrabandara49@gmail.com>
2022-04-20 22:17:56 +0530

    User Controller Initiated

---
+++ BackEndSimpleRoot/src/main/java/com/example/backendsimpleroot/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.example.backendsimpleroot.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@CrossOrigin
+public class UserController {
+
+    //get -get data
+    //post -save data
+    //put - update data
+    //delete -delete data
+    @GetMapping("/getUser")
+    public String getUser(){
+        return "Get User";
+    }
+
+    @PostMapping("/saveUser")
+    public String saveUser(){
+        return "Save User";
+    }
+
+
+    @PutMapping("/updateUser")
+    public String updateUser(){
+        return "Update User";
+    }
+
+
+    @DeleteMapping("/deleteUser")
+    public String deleteUser(){
+        return "Delete User";
+    }
+
+
+
+
+}
