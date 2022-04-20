b50f5e45eee7ecb1283dd459a802b98ccb1598c0
SachPrecious <sachithrabandara49@gmail.com>
2022-04-20 22:00:26 +0530

    Init commit

---
+++ BackEndSimpleRoot/src/main/java/com/example/backendsimpleroot/BackEndSimpleRootApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendsimpleroot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSimpleRootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackEndSimpleRootApplication.class, args);
+    }
+
+}
---
+++ BackEndSimpleRoot/src/test/java/com/example/backendsimpleroot/BackEndSimpleRootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendsimpleroot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSimpleRootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
